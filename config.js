271a9acfe9f405e79cc48bc1d9a7048a6f5b04ae
Timothy Ombun Giovanni Siahaan <timothy.og.siahaan@gmail.com>
2017-02-01 14:11:24 +0700

    delete config

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-  consumer_key:         'AxfRTwgEoMXhDrt5jAocx33wJ',
-  consumer_secret:      'mkf2zKVK4SOFaf4boBNDLGFwwvloYjjRAlyjo1Yiu5gm5Vjv2N',
-  access_token:         '147970158-35cyWiv0ub2jvpMUZdee1rOszdW8qggWQCBwmvlC',
-  access_token_secret:  'hNIuhxyfE0vebVAqUnNnQSxhoOVhweaqaogoiHS4howrt'
-}
